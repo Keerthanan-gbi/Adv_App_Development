db1a104dbf72f7bec99f1242e4f61c0f220bb851
Keerthanan-gbi <115694028+Keerthanan-gbi@users.noreply.github.com>
2024-01-27 09:37:45 +0530

    Add files via upload

---
+++ Frontend/Day 3/src/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    Home
+      
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  )
+}
+
+export default Home
